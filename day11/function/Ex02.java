889df1a75cc42b510bba04c807d236108df7b66c
SeungHoJ <125536885+SeungHoJ@users.noreply.github.com>
2023-10-26 16:14:25 +0900

    day11 학습자료

---
+++ day11/function/Ex02.java
@@ -0,0 +1,66 @@
+package function;
+
+import java.util.Scanner;
+
+public class Ex02 {
+	static String getInfo(String name,String birth) {
+		String result = "";
+		int year = Integer.parseInt(birth.substring(0,2));
+		year +=(23 < year) ? 1900 : 2000;
+		int age = 2023 - year;
+		int month = Integer.parseInt(birth.substring(2,4));
+		int date = Integer.parseInt(birth.substring(4));
+		
+//		System.out.printf("%s님은 %d살이고, %4d-%02d-%02d 출생입니다",name,age,year,month);
+		String form = "%s님은 %d살이고, %4d-%02d-%02d 출생입니다";
+		result = String.format(form, name,age,year,month,date);
+		
+		return result;
+	}
+	static boolean isNumeric(String str) {
+		boolean flag = true;
+		
+		for(int i = 0; i < str.length();i++) {
+			char ch = str.charAt(i);
+			if(('0'<=ch&&ch<='9')==false) {
+				return false;		// 반복문 내부에서도 return을 만나면 반복을 중단하고 함수를 종료한다
+			}
+		}
+		return flag;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// 이름과 생년월일을입력받아서
+		// 이름, 올해나이, 생일을 yyyy-mm-dd형식으로 출력하는 코드를 작성하기
+		
+		
+		Scanner sc = new Scanner(System.in);
+		String name;
+		String birth;
+		String result;
+		
+		System.out.print("이름 입력 : ");
+		name = sc.nextLine();
+		
+		while(true) {
+			System.out.print("생년월일 입력(yymmdd) : ");
+			birth = sc.nextLine();
+			if(birth.length() == 6 && isNumeric(birth))break;
+			System.out.println("생년월일은 6자리의 정수로 입력해주세요");
+		}
+		
+		result = getInfo(name, birth);
+		System.out.println(result);
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+}
