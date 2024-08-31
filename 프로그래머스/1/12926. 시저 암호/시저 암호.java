973cab83987cff4fbc6a6db323c583152a889143
SeungHoJ <125536885+SeungHoJ@users.noreply.github.com>
2024-08-31 14:21:58 +0900

    [level 1] Title: 시저 암호, Time: 28.07 ms, Memory: 99.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        for(int i = 0; i < s.length(); i++){
+            if('a' <= s.charAt(i)&& s.charAt(i) <= 'z'){
+                if(s.charAt(i)+n > 'z'){
+                    answer += (char)(s.charAt(i)+n-26);
+                }else{
+                    answer += (char)(s.charAt(i)+n);
+                }
+            }
+            else if('A' <= s.charAt(i)&& s.charAt(i) <= 'Z'){
+                if(s.charAt(i)+n > 'Z'){
+                    answer += (char)(s.charAt(i)+n-26);
+                }else{
+                    answer += (char)(s.charAt(i)+n);
+                }
+            }else{
+                answer += " ";
+            }
+            
+        }
+        return answer;
+    }
+}
